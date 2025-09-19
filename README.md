f556ce0a7f0a7c8642e3166f4831f6607fbff8a7
Ryan Davidson <52106678+rdsngit@users.noreply.github.com>
2025-09-19 11:47:56 +0100

    Add zshrc and setup instructions

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # dotfiles
+
+## Zsh
+
+Then amend your `~/.zshrc` to source the one in the dotfiles foler, eg
+
+```sh
+source ~/dotfiles/zshrc.sh
+```
---
+++ zshrc.sh
@@ -0,0 +1,24 @@
+# zshrc.sh
+
+# Z SHELL CONFIGURATION
+
+# Instructions: amend the local ~/.zshrc with the line: source ~/dotfiles/zshrc.sh
+# NOTE: the .sh file extension is so it is identified as a shell script
+
+
+# ------------------------ #
+
+
+# PRINT MESSAGE
+
+MESSAGE="%F{14}
+     _/_/_/_/_/            _/
+         _/      _/_/_/  _/_/_/
+      _/      _/_/      _/    _/
+   _/            _/_/  _/    _/
+_/_/_/_/_/  _/_/_/    _/    _/
+%f"
+print -P ${MESSAGE}
+
+
+# --- END --- #
